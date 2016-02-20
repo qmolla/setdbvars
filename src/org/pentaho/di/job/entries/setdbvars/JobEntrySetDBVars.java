77d63a85f2408b2e4092f9d60afeff1d945810ed
quique <quique@Aralex>
2016-02-20 12:04:55 +0100

    goin on

--- src/org/pentaho/di/job/entries/setdbvars/JobEntrySetDBVars.java
+++ src/org/pentaho/di/job/entries/setdbvars/JobEntrySetDBVars.java
@@ -3,6 +3,7 @@ package org.pentaho.di.job.entries.setdbvars;
 import org.pentaho.di.core.Result;
 import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.job.entry.JobEntryBase;
 import org.pentaho.di.job.entry.JobEntryInterface;
 
@@ -15,7 +16,20 @@ public class JobEntrySetDBVars extends JobEntryBase implements Cloneable, JobEnt
 	private Boolean useVariableSubstitution = false;
 	private String sql;
 
+	public int fileVariableType;
+
+	public static final int VARIABLE_TYPE_JVM = 0;
+	public static final int VARIABLE_TYPE_CURRENT_JOB = 1;
+	public static final int VARIABLE_TYPE_PARENT_JOB = 2;
+	public static final int VARIABLE_TYPE_ROOT_JOB = 3;
 	
+	private static final String[] variableTypeCode = { "JVM", "CURRENT_JOB", "PARENT_JOB", "ROOT_JOB" };
+	  private static final String[] variableTypeDesc = {
+	    BaseMessages.getString( PKG, "SetDBVars.VariableType.JVM" ),
+	    BaseMessages.getString( PKG, "SetDBVars.VariableType.CurrentJob" ),
+	    BaseMessages.getString( PKG, "SetDBVars.VariableType.ParentJob" ),
+	    BaseMessages.getString( PKG, "SetDBVars.VariableType.RootJob" ), };
+
 
 	public JobEntrySetDBVars(String n)
 	{
@@ -59,5 +73,42 @@ public class JobEntrySetDBVars extends JobEntryBase implements Cloneable, JobEnt
 	public void setSql(String sql) {
 		this.sql = sql;
 	}
+	
+	public static final String getVariableTypeDescription( int variableType ) {
+		    return variableTypeDesc[variableType];
+	}
+	
+	public static final String[] getVariableTypeDescriptions() {
+		    return variableTypeDesc;
+	}
+
+	public static final int getVariableType( String variableType ) {
+		    for ( int i = 0; i < variableTypeCode.length; i++ ) {
+		      if ( variableTypeCode[i].equalsIgnoreCase( variableType ) ) {
+		        return i;
+		      }
+		    }
+		    for ( int i = 0; i < variableTypeDesc.length; i++ ) {
+		      if ( variableTypeDesc[i].equalsIgnoreCase( variableType ) ) {
+		        return i;
+		      }
+		    }
+		    return VARIABLE_TYPE_JVM;
+	}
+	
+	  /**
+	   * @return the fileVariableType
+	   */
+	  public int getFileVariableType() {
+	    return fileVariableType;
+	  }
+
+	  /**
+	   * @param fileVariableType
+	   *          the fileVariableType to set
+	   */
+	  public void setFileVariableType( int fileVariableType ) {
+	    this.fileVariableType = fileVariableType;
+	  }
 	
 }
--- src/org/pentaho/di/ui/job/entries/setdbvars/JobEntrySetDBVarsDialog.java
+++ src/org/pentaho/di/ui/job/entries/setdbvars/JobEntrySetDBVarsDialog.java
@@ -6,22 +6,29 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.Props;
+import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.job.JobMeta;
 import org.pentaho.di.job.entries.setdbvars.JobEntrySetDBVars;
 import org.pentaho.di.job.entry.JobEntryDialogInterface;
 import org.pentaho.di.job.entry.JobEntryInterface;
 import org.pentaho.di.repository.Repository;
+import org.pentaho.di.ui.core.gui.WindowProperty;
 import org.pentaho.di.ui.core.widget.StyledTextComp;
 import org.pentaho.di.ui.job.dialog.JobDialog;
 import org.pentaho.di.ui.job.entry.JobEntryDialog;
@@ -38,8 +45,11 @@ public class JobEntrySetDBVarsDialog extends JobEntryDialog implements JobEntryD
 	private FormData fdlName, fdName;
 	private Shell shell;
 	
-	private boolean changed;
+	private boolean changed;	
 	
+	private Label wlFileVariableType;
+	private CCombo wFileVariableType;
+	private FormData fdlFileVariableType, fdFileVariableType;
 	
 	private Label wlUseSubs;
 	private Button wUseSubs;
@@ -54,7 +64,9 @@ public class JobEntrySetDBVarsDialog extends JobEntryDialog implements JobEntryD
 	private FormData fdlSQL, fdSQL;
 	
 	private Button wOK, wCancel;
+	private Listener lsOK, lsCancel;
 
+	private SelectionAdapter lsDef;
 
 	
 	public JobEntrySetDBVarsDialog(Shell parent, JobEntryInterface jobEntry, Repository rep, JobMeta jobMeta) {
@@ -65,6 +77,7 @@ public class JobEntrySetDBVarsDialog extends JobEntryDialog implements JobEntryD
 	}
 
 	
+	@SuppressWarnings("static-access")
 	@Override
 	public JobEntryInterface open() {
 		
@@ -91,14 +104,25 @@ public class JobEntrySetDBVarsDialog extends JobEntryDialog implements JobEntryD
 		
 	    shell.setLayout( formLayout );
 	    shell.setText( BaseMessages.getString( PKG, "JobSQL.Title" ) );
-
-	    wOK = new Button( shell, SWT.PUSH );
-	    wOK.setText( BaseMessages.getString( PKG, "System.Button.OK" ) );
-	    wCancel = new Button( shell, SWT.PUSH );
-	    wCancel.setText( BaseMessages.getString( PKG, "System.Button.Cancel" ) );
-
-	    BaseStepDialog.positionBottomButtons( shell, new Button[] { wOK, wCancel }, margin, null );
 	    
+	    
+	    // Name line
+	    wlName = new Label( shell, SWT.RIGHT );
+	    wlName.setText( BaseMessages.getString( PKG, "SetDBVars.Name.Label" ) );
+	    props.setLook( wlName );
+	    fdlName = new FormData();
+	    fdlName.left = new FormAttachment( 0, 0 );
+	    fdlName.right = new FormAttachment( middle, -margin );
+	    fdlName.top = new FormAttachment( 0, margin );
+	    wlName.setLayoutData( fdlName );
+	    wName = new Text( shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER );
+	    props.setLook( wName );
+	    wName.addModifyListener( lsMod );
+	    fdName = new FormData();
+	    fdName.left = new FormAttachment( middle, 0 );
+	    fdName.top = new FormAttachment( 0, margin );
+	    fdName.right = new FormAttachment( 100, 0 );
+	    wName.setLayoutData( fdName );
 	    
 	    // Connection line
 	    wConnection = addConnectionLine( shell, wName, middle, margin );
@@ -106,6 +130,25 @@ public class JobEntrySetDBVarsDialog extends JobEntryDialog implements JobEntryD
 	      wConnection.select( 0 );
 	    }
 	    wConnection.addModifyListener( lsMod );
+	    
+	    // file variable type line
+	    wlFileVariableType = new Label( shell, SWT.RIGHT );
+	    wlFileVariableType.setText( BaseMessages.getString( PKG, "JobEntrySetVariables.FileVariableType.Label" ) );
+	    props.setLook( wlFileVariableType );
+	    fdlFileVariableType = new FormData();
+	    fdlFileVariableType.left = new FormAttachment( 0, 0 );
+	    fdlFileVariableType.right = new FormAttachment( middle, -margin );
+	    fdlFileVariableType.top = new FormAttachment( wConnection, margin );
+	    wlFileVariableType.setLayoutData( fdlFileVariableType );
+	    wFileVariableType = new CCombo( shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER | SWT.READ_ONLY );
+	    props.setLook( wFileVariableType );
+	    wFileVariableType.addModifyListener( lsMod );
+	    fdFileVariableType = new FormData();
+	    fdFileVariableType.left = new FormAttachment( middle, 0 );
+	    fdFileVariableType.top = new FormAttachment( wConnection, margin );
+	    fdFileVariableType.right = new FormAttachment( 100, 0 );
+	    wFileVariableType.setLayoutData( fdFileVariableType );
+	    wFileVariableType.setItems( jobEntry.getVariableTypeDescriptions() );
 	    
 	    // Use variable substitution?
 	    wlUseSubs = new Label( shell, SWT.RIGHT );
@@ -161,6 +204,110 @@ public class JobEntrySetDBVarsDialog extends JobEntryDialog implements JobEntryD
 	    wSQL.setLayoutData( fdSQL );
 	    
 	    
-		return null;
+	    wOK = new Button( shell, SWT.PUSH );
+	    wOK.setText( BaseMessages.getString( PKG, "System.Button.OK" ) );
+	    wCancel = new Button( shell, SWT.PUSH );
+	    wCancel.setText( BaseMessages.getString( PKG, "System.Button.Cancel" ) );
+
+	    BaseStepDialog.positionBottomButtons( shell, new Button[] { wOK, wCancel }, margin, null );
+	    
+	    // Add listeners
+	    lsCancel = new Listener() {
+	      public void handleEvent( Event e ) {
+	        cancel();
+	      }
+	    };
+	    lsOK = new Listener() {
+	      public void handleEvent( Event e ) {
+	        ok();
+	      }
+	    };
+
+	    wCancel.addListener( SWT.Selection, lsCancel );
+	    wOK.addListener( SWT.Selection, lsOK );
+
+	    lsDef = new SelectionAdapter() {
+	      public void widgetDefaultSelected( SelectionEvent e ) {
+	        ok();
+	      }
+	    };
+
+	    wName.addSelectionListener( lsDef );
+
+
+	    // Detect X or ALT-F4 or something that kills this window...
+	    shell.addShellListener( new ShellAdapter() {
+	      public void shellClosed( ShellEvent e ) {
+	        cancel();
+	      }
+	    } );
+
+	    getData();
+
+	    BaseStepDialog.setSize( shell );
+
+	    shell.open();
+	    while ( !shell.isDisposed() ) {
+	      if ( !display.readAndDispatch() ) {
+	        display.sleep();
+	      }
+	    }	    
+	    
+		return jobEntry;
 	}
+	
+	
+	  /**
+	   * Copy information from the meta-data input to the dialog fields.
+	   */
+	  @SuppressWarnings("static-access")
+	public void getData() {
+	    wName.setText( Const.nullToEmpty( jobEntry.getName() ) );
+	    wSQL.setText( Const.nullToEmpty( jobEntry.getSql() ) );
+	    DatabaseMeta dbinfo = jobEntry.getDatabase();
+	    if ( dbinfo != null && dbinfo.getName() != null ) {
+	      wConnection.setText( dbinfo.getName() );
+	    } else {
+	      wConnection.setText( "" );
+	    }
+
+	    wUseSubs.setSelection( jobEntry.getUseVariableSubstitution() );
+
+	    wFileVariableType.setText( jobEntry.getVariableTypeDescription( jobEntry.getFileVariableType() ) );
+
+	    wName.selectAll();
+	    wName.setFocus();
+	  }
+	
+	public void dispose() {
+		    WindowProperty winprop = new WindowProperty( shell );
+		    props.setScreen( winprop );
+		    shell.dispose();
+	}
+	
+	
+	 private void cancel() {
+		    jobEntry.setChanged( changed );
+		    jobEntry = null;
+		    dispose();
+	}
+	 
+	 
+	@SuppressWarnings("static-access")
+	private void ok() {
+	   if ( Const.isEmpty( wName.getText() ) ) {
+	     MessageBox mb = new MessageBox( shell, SWT.OK | SWT.ICON_ERROR );
+	     mb.setText( BaseMessages.getString( PKG, "System.StepJobEntryNameMissing.Title" ) );
+	     mb.setMessage( BaseMessages.getString( PKG, "System.JobEntryNameMissing.Msg" ) );
+	     mb.open();
+	     return;
+	   }
+	   jobEntry.setName( wName.getText() );
+	   jobEntry.setSql(wSQL.getText() );
+	   jobEntry.setUseVariableSubstitution( wUseSubs.getSelection() );
+	   jobEntry.setFileVariableType( jobEntry.getVariableType( wFileVariableType.getText() ) );
+	   jobEntry.setDatabase( jobMeta.findDatabase( wConnection.getText() ) );
+	   dispose();
+	} 
+	 
 }
